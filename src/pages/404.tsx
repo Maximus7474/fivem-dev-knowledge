99fa09e9704dd7c554a8e64ccd528168315118c4
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-24 20:00:59 +0100

    rf(404): removed unused vars & imports

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,7 @@
-import { Button, Title, Text, Center, Image, useMantineColorScheme } from '@mantine/core';
+import { Button, Title, Center } from '@mantine/core';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
-    const { colorScheme } = useMantineColorScheme()
-
     return (
         <Center style={{ flexDirection: 'column', padding: '2em', margin: 'auto 0' }}>
             <Title order={1} style={{ fontSize: '3rem' }}>
